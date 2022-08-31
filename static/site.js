a950eb46a1570bdbae8d895ba924a379b0fe2bf3
viswajith <viswajithroop1@gmail.com>
2022-08-31 11:09:12 +0530

    main updation reloading the link after ajax reloading the sidebar's link afer ajax changed side bar color on css

--- static/site.js
+++ static/site.js
@@ -1,3 +1,6 @@
+
+
+
 function build_lyrics(song){
     ret = $(`<p>
     <h4> Lyrics for ${song.name} </h4>
@@ -10,19 +13,46 @@ function build_lyrics(song){
 }
 
 function main() {
-    console.log("Hello world! I am loaded!");
-    $("a.songLink").click(function (ev) {
-        console.log(ev.target.href);
-        $("div.lyrics").text("Loading ... ");
-        $.ajax({url : ev.target.href,
-                dataType: 'json',
-                success: function(data, textStatus, jqXHR) {    
-                    console.log(data)
-                    $("div.lyrics").html(build_lyrics(data.song));
-                },    
-            });
-        ev.preventDefault();
-        });
+    console.log("Hello!!!!!")
+    $("a.songLink").click(myFunction)
+};
+function myFunction(ev) {
+    ev.preventDefault();
+    $("div.lyrics").text("Loading......")
+    $.ajax({
+        url: ev.target.href,
+        dataType: 'json',
+        success: function (data, textStatus, jqXHR) {
+            $("div.lyrics").html(build_lyrics(data.song));
+            var text = ev.target.innerText;
+            var parent = ev.target.parentNode;
+            $(parent).html(text)
+            $(".songname1")
+                .html(`<a class = "songLink" href="/song/${$(".songname1")
+                .attr("id")}">${$(".songname1").text()}<a/>`);
+            $(".songname1 a").click(myFunction);
+            $(".songname1").attr("class", "songname2");
+            $(parent).attr("class", "songname1");
+        }
+    })
 }
 $(main);
+
+
+// function main() {
+//     console.log("Hello world! I am loaded!");
+//     $("a.songLink").click(function (ev) {
+//         console.log(ev.target.href);
+//         $("div.lyrics").text("Loading ... ");
+//         $.ajax({url : ev.target.href,
+//                 dataType: 'json',
+//                 success: function(data, textStatus, jqXHR) {    
+//                     console.log(data)
+//                     $("div.lyrics").html(build_lyrics(data.song));
+//                 },    
+//             });
+//         ev.preventDefault();
+//         });
+// }
+// $(main);
 
